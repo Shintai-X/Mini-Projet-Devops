b5d3a5532a3e1dc841b6d3f3dfb6af51ea7565c9
Nizar <seffarnizar.pro@gmail.com>
2023-04-25 18:59:23 +0100

    lll

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
 const { exec } = require('child_process');
 
-const gitInitPromise = new Promise((resolve, reject) => {
-  exec('git init', (error, stdout, stderr) => {
-    if (error) {
-      reject(`Git init error: ${error}`);
-    } else {
-      resolve(stdout);
-    }
-  });
-});
+exec('console');
+exec('node index.js')
+exec('git init');
 
-const gitPullPromise = new Promise((resolve, reject) => {
-  exec('git pull https://github.com/Shintai-X/Mini-Projet-Devops.git main', (error, stdout, stderr) => {
-    if (error) {
-      reject(`Git pull error: ${error}`);
-    } else {
-      resolve(stdout);
-    }
-  });
-});
+exec('git pull https://github.com/Shintai-X/Mini-Projet-Devops.git main', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Git pull error: ${error}`);
+    return;
+  }
 
-Promise.all([gitInitPromise, gitPullPromise]).then(([gitInitOutput, gitPullOutput]) => {
-  console.log(`Git init stdout: ${gitInitOutput}`);
-  console.log(`Git pull stdout: ${gitPullOutput}`);
-}).catch((error) => {
-  console.error(error);
+  console.log(`Git pull stdout: ${stdout}`);
+  console.log(`Git pull stderr: ${stderr}`);
+  
 });
 
