1686576fd043fb58fb3649699c6dd08852f69569
Nizar <seffarnizar.pro@gmail.com>
2023-04-25 19:04:31 +0100

    lll

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 
 
-
+exec('git init');
 exec('git pull https://github.com/Shintai-X/Mini-Projet-Devops.git main', (error, stdout, stderr) => {
   if (error) {
     console.error(`Git pull error: ${error}`);
